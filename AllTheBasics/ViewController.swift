faad930a6a9f43090ae5bddf28820030fbdbeaa0
Amar Cheema <amar.s.cheema@gmail.com>
2018-07-27 23:29:57 -0400

    Completed Problems

--- AllTheBasics/ViewController.swift
+++ AllTheBasics/ViewController.swift
@@ -10,14 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    
-    
-    
-    
     // Implement your functions here
     
-    
-    
-    
+    func averageIsAbove75(first a:Double, second b:Double, third c:Double)->Bool {
+        let average:Double = (a*b*c)/3
+        
+        switch average > 75 {
+        
+        case true:
+            return true
+        default:
+            return false
+        }
+    }
 
+    func passWordCombo(firstEntry userName:String, secondEntry password:Int) {
+        switch (userName, password%3) {
+        case ("Jerry", 0):
+            print ("Welcome")
+        case ("Elaine", 0):
+            print ("Welcome")
+        case ("Michael", 0):
+            print ("Welcome")
+        default:
+            print ("Access Denied")
+        }
+    }
+
+    func describe(picture emoji:String)->String {
+        switch emoji {
+        case "💋":
+            return "Kiss"
+        case "🐈":
+            return "Cat"
+        case "🐢":
+            return "Turtle"
+        case "🍕":
+            return "Pizza"
+        case "👻":
+            return "Ghost"
+        default:
+            return "Uknown"
+        }
+    }
 }
+
